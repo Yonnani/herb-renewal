a53af69fba48c84d3305c844cc21195d6f9567ea
Yonnani <iamai5206@gmail.com>
2018-08-12 13:10:18 +0900

    이미지 추가

--- src/vue/Home.vue
+++ src/vue/Home.vue
@@ -12,11 +12,17 @@
                     @sliding-start="onSlideStart"
                     @sliding-end="onSlideEnd">
 
-            <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=52"></b-carousel-slide>
+            <b-carousel-slide>
+                <img slot="img" class="d-block img-fluid w-100"
+                     src="./../assets/img/main_01.jpg" alt="image slot">
+            </b-carousel-slide>
 
             <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54"></b-carousel-slide>
 
-            <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+            <b-carousel-slide>
+                <img slot="img" class="d-block img-fluid w-100"
+                     src="./../assets/img/main_04.jpg" alt="image slot">
+            </b-carousel-slide>
 
             <!-- Slides with img slot -->
             <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
@@ -36,7 +42,7 @@
                 <a href="#/portfolio">더보기</a>
             </div>
             <div class="d-flex justify-content-between m-3" style="height: 150px">
-                <img src="https://picsum.photos/1024/480/?image=52" style="width: 25%"/>
+                <img src="./../assets/img/main_01.jpg" style="width: 25%"/>
                 <img src="https://picsum.photos/1024/480/?image=54" style="width: 25%"/>
                 <img src="https://picsum.photos/1024/480/?image=58" style="width: 25%"/>
                 <img src="https://picsum.photos/1024/480/?image=55" style="width: 25%"/>
