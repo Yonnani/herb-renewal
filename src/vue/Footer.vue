50d1ed61e9f3364c25677098b212838dd5d1906e
Yonnani <iamai5206@gmail.com>
2018-08-12 19:12:34 +0900

    푸터 내용 추가

--- src/vue/Footer.vue
+++ src/vue/Footer.vue
@@ -1,5 +1,27 @@
 <template>
-    <h3>This is Footer</h3>
+    <div>
+        <b-row>
+            <b-col>
+                <img src="./../assets/logo.png" />
+            </b-col>
+            <b-col>
+                <p>
+                    <a to="company">인사말</a> / <a href="">오시는길</a> / <a href="">견적기타문의</a> / 이메일주소무단수집금지
+                </p>
+                <p>
+                    주식회사 허브동산, 사업자등록번호 : 617-89-18001 대표 장기웅<br>
+                    본사 : 부산광역시 중앙대로 2093, 803호<br>
+                    E-mail : herb54321@naver.com / FAX 051)517-1755<br>
+                </p>
+            </b-col>
+            <b-col>
+                <p>
+                    전화문의<br>
+                    051)902-1755
+                </p>
+            </b-col>
+        </b-row>
+    </div>
 </template>
 
 <script>
--- src/vue/Home.vue
+++ src/vue/Home.vue
@@ -17,22 +17,20 @@
                      src="./../assets/img/main_01.jpg" alt="image slot">
             </b-carousel-slide>
 
-            <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54"></b-carousel-slide>
+            <b-carousel-slide>
+                <img slot="img" class="d-block img-fluid w-100"
+                     src="./../assets/img/main_02.jpg" alt="image slot">
+            </b-carousel-slide>
+
+            <b-carousel-slide>
+                <img slot="img" class="d-block img-fluid w-100"
+                     src="./../assets/img/main_03.jpg" alt="image slot">
+            </b-carousel-slide>
 
             <b-carousel-slide>
                 <img slot="img" class="d-block img-fluid w-100"
                      src="./../assets/img/main_04.jpg" alt="image slot">
             </b-carousel-slide>
-
-            <!-- Slides with img slot -->
-            <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-            <b-carousel-slide>
-                <img slot="img" class="d-block img-fluid w-100" width="1024" height="480"
-                     src="https://picsum.photos/1024/480/?image=55" alt="image slot">
-            </b-carousel-slide>
-
-            <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-            <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image"></b-carousel-slide>
 
         </b-carousel>
 
@@ -43,9 +41,9 @@
             </div>
             <div class="d-flex justify-content-between m-3" style="height: 150px">
                 <img src="./../assets/img/main_01.jpg" style="width: 25%"/>
-                <img src="https://picsum.photos/1024/480/?image=54" style="width: 25%"/>
-                <img src="https://picsum.photos/1024/480/?image=58" style="width: 25%"/>
-                <img src="https://picsum.photos/1024/480/?image=55" style="width: 25%"/>
+                <img src="./../assets/img/main_02.jpg" style="width: 25%"/>
+                <img src="./../assets/img/main_03.jpg" style="width: 25%"/>
+                <img src="./../assets/img/main_04.jpg" style="width: 25%"/>
             </div>
         </div>
     </div>
